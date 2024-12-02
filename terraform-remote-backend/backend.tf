d4d0c5588c0c9370b81435fff00ac2aea6348a03
UnwillingElf <register_matthew@yahoo.com>
2024-12-02 17:09:32 -0500

    change

--- terraform-remote-backend/backend.tf
+++ terraform-remote-backend/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket = "w6-terraform-bucket"
+    bucket = "w6-34234324terraform-wseffweewfbucket"
     key    = "dev/terraform.tfstate"
     region = "us-east-1"
-    dynamodb_table = "statefile-lock"
+    dynamodb_table = "554523542statefile-erwererew-lock"
     encrypt = true
   }
 }
